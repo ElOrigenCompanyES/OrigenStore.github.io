8e75705b6b4347f1310dc9aa08a0cf6b96a11eb5
zKXDEX <deiividdlk@gmail.com>
2023-08-29 19:38:56 +0200

    origen doc

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Origen Store</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.gg/5tWchb759Z',
+  },
+  docsRepositoryBase: 'https://github.com/IceSystem',
+  footer: {
+    text: 'Nextra Docs',
+  },
+}
+
+export default config
