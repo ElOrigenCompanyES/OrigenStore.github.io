31166572ed8c9a0b121ae82afebc61ad0500f890
zKXDEX <deiividdlk@gmail.com>
2023-08-30 16:15:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,4 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra({
-  images: {
-    unoptimized: true
-  }
-});
-
-// module.exports = withNextra()
+module.exports = withNextra()
